173e9196f33d2e797cc05699616f4bced7e93e00
alison-acuna <aacuna136@gmail.com>
2017-05-29 18:37:08 -0700

    Built in if/else to account for different messages. NS: Integrate into code re moisture sensor

--- Moisture-Sensor/tweet.py
+++ Moisture-Sensor/tweet.py
@@ -1,9 +1,27 @@
 import tweepy
 
+# This is the message that will be sent when moisture IS NOT detected
+
+message_dead = "Muffet says: I am thirsty!  Water please @Alison_Acuna"
+
+# This is the message that will be sent when moisture IS detected again
+
+message_alive = "Muffet says: Thanks! All hydrated and happy @Alison_Acuna"
+
+plant_status = 1
+
 def get_api(cfg):
   auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
   auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
   return tweepy.API(auth)
+
+def set_tweet(plant_status):
+	if plant_status == 1:
+		tweet = message_dead
+	else:
+		tweet = message_alive
+	return tweet
+
 
 def main():
   # Fill in the values noted in previous step here
@@ -15,7 +33,7 @@ def main():
     }
 
   api = get_api(cfg)
-  tweet = "My first Twitter App is tweeting Successfully"
+  tweet = set_tweet(1)
   status = api.update_status(status=tweet)
   # Yes, tweet is called 'status' rather confusing
 
